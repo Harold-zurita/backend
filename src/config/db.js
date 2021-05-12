7fc678d8547fefc7a47c1e10dbe1ed04bd5abc85
Harold-zurita <83727037+Harold-zurita@users.noreply.github.com>
2021-05-11 22:49:45 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,10 @@ const MariaConexion = () => {
 
     return mysql.createConnection({
         
-        host: process.env.HOST_DB,
-        user: process.env.USER_DB,
-        password: process.env.PASS_DB,
-        database: process.env.DB_NAME
+        host: "mysql-antoniomonje.alwaysdata.net",
+        user: "234882_harold",
+        password: "zurita1024!",
+        database: "antoniomonje_menugement_db"
         
     });
 
