22e14b5d0a289910b5e410ac7e5efa50a53138a1
Harold-zurita <83727037+Harold-zurita@users.noreply.github.com>
2021-05-11 23:51:42 -0400

    Update Recetas.js

--- src/controllers/Recetas.js
+++ src/controllers/Recetas.js
@@ -5,75 +5,54 @@ class RecetaController{
 
     RecetaGet(req, res){
 
-        const db = MariaConexion();
-
-        db.connect();
-
         const querySQL = `SELECT * FROM receta`;
         
-        db.query(querySQL, (err, rows, fields)=>{
-            
-            if(err){
-                
-                console.log(err);
-                throw new Error("La consulta esta fallando");
+        MariaConexion.query(querySQL, (err, rows, fields)=>{
 
+            if(err){
+                throw new Error("La consulta esta fallando");
             }
 
             rows.map(function(item, pos){
-
-                console.log(item)
-
+                //console.log(item)
             })
-
-            console.log(rows.IMAGEN);
             
             res.json({rows});
 
         });
-
-        db.end();
-
+        //db.end();
     }
 
     async RegistrarRecetas(req, res){
 
-        const db = MariaConexion();
+        const{imagen, nombReceta, dificultad, categoria,ingredientes, preparacion, tiempoPrepa} = req.body; 
+        // ConexionDB.connect Esto ya esta incluido en db, comenta esta linea en todos lados
+        MariaConexion.query(`SELECT * FROM receta WHERE NOMBRERECETA = "${nombReceta}"`, async(error, results, fields) => {
+            if (error){
+            console.log(error);
+        }else{
 
-        //console.log(req.body);
-        const{imagen, nombReceta, dificultad, categoria,ingredientes, preparacion, tiempoPrepa} = req.body
-        
-        db.connect();
+            if(results.length != 0){
+                return res.json({
+                    errors: [
+                        {
+                            msg: "El nombre de la receta ya existe"
+                        }
+                    ]
+                });
 
-        const querySQL = `INSERT INTO receta VALUES(null, "${nombReceta}", "${dificultad}", "${tiempoPrepa+' minutos'}", "${categoria}", "${imagen}", "${preparacion}", "${ingredientes}")`;
-        
-        db.query(querySQL, (err, rows, fields)=>{
-            
-            if(err){
+            }else{
 
-                console.log(err);
-                throw new Error("La consulta esta fallando");
-
+                MariaConexion.query(`INSERT INTO receta VALUES (null, "${nombReceta}", "${dificultad}", "${tiempoPrepa+' minutos'}", "${categoria}", "${imagen}", "${preparacion}", "${ingredientes}")`, function (error, results, fields) {
+                    if (error) throw error;
+                    return res.json(results);
+                });
             }
 
-            console.log(rows);
-
-            res.json({
-
-                nombReceta,
-                dificultad,
-                tiempoPrepa,
-                categoria,
-                imagen,
-                preparacion,
-                ingredientes,
-
-            });
-
-        });
-
-        db.end();
-
+        }
+        
+    });
+    //ConexionDB.end(); Comenta el end
     }
 
     uploadImage(req, res){
@@ -82,37 +61,32 @@ class RecetaController{
 
             return res.json({
                 mensaje: "Error"
-
             });
 
         }
-        
 
         const {file} = req.files;
 
         let separar = (file.name).split('.');
-        console.log(separar);
+
         const extension = separar[separar.length-1];
+
         if(!(extension ==='jpg' || extension ==='JPEG' || extension ==='png' || extension ==='JPG' || extension ==='PNG' || extension === 'jpeg')){
             return res.json({
                 errors: [{
                     msg: 'solo se permite imagenes tipo jpg, png, JPEG'
                 }]
             });
-    
         }
-        console.log(file);
+
         const dir = path.join(__dirname, "../images", file.name)
-        console.log(dir);
     
         file.mv(dir, (err) => {
 
             if(err){
 
-                console.log(err);
                 return res.json({
                     mensaje: "Error"
-
                 });
 
             }
